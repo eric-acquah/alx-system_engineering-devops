aa1658f889f01ae5a56fc039e402a6603c1d10e1
Eric Acquah <ericacquah1720@gmail.com>
2023-11-28 14:37:48 +0000

    Trying a require resourse

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -6,7 +6,8 @@ package { 'python3-pip':
 
 # Install Flask version 2.1.0
 
-package { 'Flask':
+package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => 'package[python3-pip]'
 }
