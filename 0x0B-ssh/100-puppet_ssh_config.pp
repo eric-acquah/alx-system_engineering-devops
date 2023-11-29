f268fc0b2416d2d36525eb7654efa821f0478f90
Eric Acquah <ericacquah1720@gmail.com>
2023-11-29 14:37:58 +0000

    Using a diffirent path to config file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,17 +1,10 @@
-# make sure dir .ssh exist
-
-file { "${facts['home']}/.ssh":
-  ensure => 'directory',
-}
-
-
 # configure ssh file
 
-file { "${facts['home']}/.ssh/config":
-  ensure  => 'file',
+file { "/etc/ssh/ssh_config":
+  ensure  => 'present',
   content => "Host 107.23.160.42
     User ubuntu
-    IdentityFile ${facts['home']}/.ssh/school
+    IdentityFile ~/.ssh/school
     PubkeyAuthentication yes
     PasswordAuthentication no",
 }
