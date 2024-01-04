4b9f4306c362d93acbea0a60375dcda54e6c9c5a
Eric Acquah <ericacquah1720@gmail.com>
2024-01-04 21:58:57 +0000

    Export to csv format

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+"""
+Export employee's extracted TODO data from an api to CSV format
+
+"""
+
+import csv
+import requests
+import sys
+
+
+# Get employeeId
+try:
+    uid = int(sys.argv[1])
+except Exception:
+    print("Usage: ./0-gather_data_from_an_API.py <EMPLOYEE_ID>")
+
+# Fetch for the todos
+response1 = requests.get('https://jsonplaceholder.typicode.com/todos/')
+
+
+# Fetch for name of employee
+response2 = requests.get(f'https://jsonplaceholder.typicode.com/users/{uid}')
+
+# Deserialize json object for extraction
+userName = response2.json()
+todos = response1.json()
+
+
+# Extract data
+userName = userName.get('username')
+
+filename = f"{uid}.csv"
+
+# csv fieldnames = ["USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"]
+
+with open(filename, 'w', newline="") as csvfile:
+    csvWriter = csv.writer(csvfile)
+
+    # Extract and write data into the csv file
+    for task in todos:
+        if task.get('userId') == uid:
+            taskData = []
+            taskData.append(uid)
+            taskData.append(userName)
+            taskData.append(task.get('completed'))
+            taskData.append(task.get('title'))
+            csvWriter.writerow(taskData)
