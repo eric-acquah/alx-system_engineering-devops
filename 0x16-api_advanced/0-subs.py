8aa4986711f31de7057e84780f48a9769cdf7597
Eric Acquah <ericacquah1720@gmail.com>
2024-01-19 14:10:01 +0000

    Fetch data from subreddit api

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""
+Fetch data from the subreddit API
+
+"""
+
+import praw
+
+
+def number_of_subscribers(subreddit):
+    """
+    Fetch the total number of subscribers of a given subreddit
+
+    Args:
+        subreddit (obj) - instance of the reddit api object obtined from praw
+
+    Return:
+        Number of subscribers of the given subreddit else return 0
+    """
+
+    reddit = praw.Reddit(client_id="o5LcaoRyV02tUzF9tZZ7Xg",
+                         client_secret="Pzt78Mz5WZR6AMpfEJsOtmuZJS30rA",
+                         user_agent="myRedditScript/v1.0 (by /u/Eric)")
+
+    subreddit = reddit.subreddit(subreddit)
+
+    try:
+        return subreddit.subscribers
+
+    except Exception:
+        return 0
